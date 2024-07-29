a4b9df571e1e576d6e39143f7c8efd9b6dfe2a0e
Matheus Fernandes <matheusfer200@gmail.com>
2024-07-28 21:09:30 -0300

    update

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const path = require('node:path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
+    width: 1080,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
